237e79acc7e69cd2dfcdf6dab026f2ac65a70d84
daniilnahl <daniil.nahl@protonmail.com>
2024-10-25 14:30:42 -0700

    v0.00.2
    Implemented levels menu.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,6 +1,53 @@
 import java.util.*;
 
 public class MainMenu {
+    public static void levelMenu(Scanner console) {
+        //variables
+        boolean exitLevelMenu = false;
+        String levelMenu = """
+        Levels Available
+        1: Level 1
+        2: Level 2
+        3: Level 3
+        4: Level 4
+        5: Exit
+        """;
+
+        System.out.println(levelMenu);
+        //level menu loop
+        while (!exitLevelMenu){
+            try {
+                System.out.print("Enter the level number: ");
+                int user_input = console.nextInt();
+
+                if (user_input == 1){
+                    System.out.println("Level 1");  
+                }
+                else if (user_input == 2){
+                    System.out.println("Level 2");
+                }
+                else if (user_input == 3){
+                    System.out.println("Level 3");
+                }
+                else if (user_input == 4){
+                    System.out.println("Level 4");
+                }
+                else if (user_input == 5){
+                    System.out.println("Exiting levels menu");
+                    exitLevelMenu = true;
+                }
+                else{//for integer which are outside command numbers bound
+                    System.out.println("Invalid level number. Enter an integer number corresponding to a level.");
+                }   
+            
+            } catch (InputMismatchException e) {//Scanner built-in exception 
+                System.out.println("Invalid input type. Please enter an integer.");
+                console.next();//need this to prevent infinite loop
+            }          
+        }
+        
+    }
+
     public static void main(String[] args) {
         //variables
         boolean exitProgram = false;
@@ -9,7 +56,8 @@ public class MainMenu {
         1: Levels
         2: Leaderboard
         3: Export score
-        4: Exit 
+        4: Help
+        5: Exit 
         """;
 
         //objects
@@ -20,11 +68,11 @@ public class MainMenu {
         while (!exitProgram){
             try {
                 System.out.print("Enter the command: ");
+                
                 int user_input = console.nextInt();
 
-                if (user_input == 1){
-                    System.out.println("none");
-                    
+                if (user_input == 1){//levels
+                    levelMenu(console);  
                 }
                 else if (user_input == 2){
                     System.out.println("none");
