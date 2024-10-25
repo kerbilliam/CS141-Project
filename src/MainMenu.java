611860fa60050fe97088c2488c5077fc75ed2cca
daniilnahl <daniil.nahl@protonmail.com>
2024-10-25 14:44:06 -0700

    v0.00.3
    Reduced bloat in level menu method.
    Create a method which would start up levels based on input.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,6 +1,21 @@
 import java.util.*;
 
 public class MainMenu {
+    public static void levelSelection(int level){
+        if (level == 1){
+            //code to run drawing panel for level 1 with its engine 
+        }
+        else if (level == 2){
+            //code to run drawing panel for level 2 with its engine 
+        }
+        else if (level == 3){
+            //code to run drawing panel for level 3 with its engine 
+        }
+        else if (level == 4){
+            //code to run drawing panel for level 4 with its engine 
+        }
+    }
+
     public static void levelMenu(Scanner console) {
         //variables
         boolean exitLevelMenu = false;
@@ -20,22 +35,16 @@ public class MainMenu {
                 System.out.print("Enter the level number: ");
                 int user_input = console.nextInt();
 
-                if (user_input == 1){
-                    System.out.println("Level 1");  
+                if (user_input == 1 || user_input == 2 || user_input == 3 || user_input == 4 ){
+                    System.out.println("Selected Level " + user_input);  
+                    levelSelection(user_input);
                 }
-                else if (user_input == 2){
-                    System.out.println("Level 2");
-                }
-                else if (user_input == 3){
-                    System.out.println("Level 3");
-                }
-                else if (user_input == 4){
-                    System.out.println("Level 4");
-                }
+
                 else if (user_input == 5){
-                    System.out.println("Exiting levels menu");
+                    System.out.println("Exiting levels menu\n");
                     exitLevelMenu = true;
                 }
+
                 else{//for integer which are outside command numbers bound
                     System.out.println("Invalid level number. Enter an integer number corresponding to a level.");
                 }   
@@ -61,7 +70,7 @@ public class MainMenu {
         """;
 
         //objects
-        Scanner console = new Scanner(System.in);
+        Scanner console = new Scanner(System.in); //only one scanner object for whole app since when closing a scanner its input stream(System.in) closes for the whole app
 
         System.out.println("\nWelcome to Angry Birds Clone!\n" + commandMenu);
         //main menu loop
@@ -72,15 +81,23 @@ public class MainMenu {
                 int user_input = console.nextInt();
 
                 if (user_input == 1){//levels
+                    System.out.println();
                     levelMenu(console);  
                 }
-                else if (user_input == 2){
+
+                else if (user_input == 2){//leaderboard
                     System.out.println("none");
                 }
-                else if (user_input == 3){
+
+                else if (user_input == 3){//export scores
                     System.out.println("none");
                 }
-                else if (user_input == 4){
+
+                else if (user_input == 4){//help 
+                    System.out.println("\n" + commandMenu);
+                }
+
+                else if (user_input == 5){//quit app
                     System.out.println("Thanks for playing!");
                     exitProgram = true;
                 }
