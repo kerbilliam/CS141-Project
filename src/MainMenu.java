b48aa1a3ae31c8e5d486d7c44ca07502862db01e
daniilnahl <daniil.nahl@protonmail.com>
2024-11-23 10:55:35 -0800

    v0.0.1
    Wrote function to loop for user input for a level. Gets user input for velocity and angle, asks for confirmation.
    Made Scanner object public for whole class.
    Modified code so a menu show up after a user is done with a level.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,6 +3,8 @@ import java.util.*;
 import utils.*;
 
 public class MainMenu {
+    //public Scanner object for whole module 
+    public static Scanner console = new Scanner(System.in); //only one scanner object for whole app since when closing a scanner its input stream(System.in) closes for the whole app
     //creates an array which would store the current user's save data
     public static String[] currentSaveFile = new String[9];
     public static List<String[]> saveFiles = new ArrayList<String[]>();
@@ -57,26 +59,68 @@ public class MainMenu {
                 
                 
         }
+    //going to need to put a variable in the () for a specific level
+    public static void runLevel() {
+        boolean userInLevelLoop = true;
+        while (userInLevelLoop) {
+            try {
+                System.out.print("Enter angle: ");
+                float angle = console.nextFloat();
+                System.out.print("Enter velocity: ");
+                float velocity = console.nextFloat();
+
+                //draws panel and projected trajactory
+                //loop for confirmation
+                while (true){
+                    System.out.print("Confirm this trajectory? Yes/No: ");
+                    String confirmation = (console.next().toLowerCase());
+                    if (confirmation.equals("yes")){
+                        //draw level
+                        //stops user in level loop
+                        System.out.println();
+                        userInLevelLoop = false;
+                        break;
+                    }else if (confirmation.equals("no")){
+                        //loop back
+                        System.out.println("Alright, lets try again.");
+                        break;
+                    }
+                    else{
+                        System.out.println("Invalid confirmation command. Try again.");
+                    }
+                }
+            
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input type for velocity or angle. Please enter a numeric value.");
+                console.next();//need this to prevent infinite loop
+            }
+        }
+    }
 
     public static void levelSelection(int level){
         /**
          * Starts a level based on parameter value given.
          */
-        if (level == 1){
+        if (level == 1){           
             //code to run drawing panel for level 1 with its engine 
+            runLevel();
+            levelMenu(); //when user exits a level show them the level menu
         }
         else if (level == 2){
             //code to run drawing panel for level 2 with its engine 
+            levelMenu(); //when user exits a level show them the level menu
         }
         else if (level == 3){
             //code to run drawing panel for level 3 with its engine 
+            levelMenu(); //when user exits a level show them the level menu
         }
         else if (level == 4){
             //code to run drawing panel for level 4 with its engine 
+            levelMenu(); //when user exits a level show them the level menu
         }
     }
 
-    public static void levelMenu(Scanner console) {
+    public static void levelMenu() {
         /**
          * Shows the  user level menu commands and prompts them for input.
          */
@@ -95,17 +139,17 @@ public class MainMenu {
         //level menu loop
         while (!exitLevelMenu){
             try {
-                System.out.print("Enter the level number: ");
+                System.out.print("Enter the level command number: ");
                 int user_input = console.nextInt();
-
-                if (user_input == 1 || user_input == 2 || user_input == 3 || user_input == 4 ){
-                    System.out.println("Selected Level " + user_input);  
-                    levelSelection(user_input);
-                }
-
-                else if (user_input == 5){
+                
+                if (user_input == 5){
                     System.out.println("Exiting levels menu\n");
                     exitLevelMenu = true;
+                }
+
+                else if (user_input == 1 || user_input == 2 || user_input == 3 || user_input == 4 ){
+                    System.out.println("Selected Level " + user_input);  
+                    levelSelection(user_input);
                 }
 
                 else{//for integer which are outside command numbers bound
@@ -138,8 +182,8 @@ public class MainMenu {
         Help Menu Info
         Levels: opens a menu page to select a level. When you select a level the app launches drawing panel and prompts you to enter angle and velocity.
         """;
-        //objects
-        Scanner console = new Scanner(System.in); //only one scanner object for whole app since when closing a scanner its input stream(System.in) closes for the whole app
+        
+        
 
         //User login prompt
         userLogin(console);
@@ -154,7 +198,8 @@ public class MainMenu {
 
                 if (user_input == 1){//levels
                     System.out.println();
-                    levelMenu(console);  
+                    levelMenu();  
+                    System.out.println(commandMenu);
                 }
 
                 else if (user_input == 2){//leaderboard
