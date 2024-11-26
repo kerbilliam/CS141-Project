ba52e454897e279fd30e61ca971383c77786a152
daniilnahl <daniil.nahl@protonmail.com>
2024-11-26 13:05:34 -0800

    v0.0.1
    Most of the code merged.
    Organized by folders.
    Implemented a script in main menu which checks for user to enter a username that contains no spaces.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -10,54 +10,62 @@ public class MainMenu {
     //position of the array save in saves file 
     public static int saveFilePosition = -1;
     
-        public static void userLogin(Scanner console) throws FileNotFoundException{
-            /**
-             * Prompts the user to enter a username and if a save data with such a name exists imports it. If not creates a new save data instance.
-             */
-            boolean userLogin = false;
-            String userName = "";
+    public static void userLogin(Scanner console) throws FileNotFoundException{
+        /**
+         * Prompts the user to enter a username and if a save data with such a name exists imports it. If not creates a new save data instance.
+         */
+        boolean userLogin = false;
+        String userName = "";
     
-            while(!userLogin){
+        while(!userLogin){
+
+                boolean correctUsernameFormat = false;
+                //loop to make sure the user doesn't enter a username that contains spaces
+                while (!correctUsernameFormat) {
                     System.out.print("Enter username: ");
                     userName = console.nextLine();
-                    saveFiles = fileModule.readFromFile(); //gets save datas
-                    boolean saveFileFound = false;
-                    while (!saveFileFound) {
-                        String[] tempSaveFile = new String[9]; //used to unpack each element of the list to check for the save data name
-                        for (int i = 0; i < saveFiles.size(); i++){
-                            tempSaveFile = saveFiles.get(i);
-                            if (tempSaveFile[0].equals(userName)){//if save data with user name exists
-                                System.out.println("Save file found. Importing data...");
-                                currentSaveFile = saveFiles.get(i);//assigns the save file data(array)
-                                saveFilePosition = i; //asigns the line number of the save data, so it can be overwritten later to save user's progress when they exit the app
-                            System.out.println("Save file imported succesfully.\n");
-                            saveFileFound = true;
-                            break;
-                        }
+                    if(userName.contains(" ")){
+                        System.out.print("Enter a username that doesn't contain any spaces.\n");
+                    }else{
+                        correctUsernameFormat = true;
                     }
-                    
-                    if (!saveFileFound){
-                        System.out.println("Save file wasn't found. Creating new save file...");
-                        //if cant find create a new array with the username and store it in the file
-                        currentSaveFile[0] = userName;
-                        currentSaveFile[1] = "Incomplete";  //level 1
-                        currentSaveFile[2] = "0";   //level 1 score
-                        currentSaveFile[3] = "Incomplete";  //level 2
-                        currentSaveFile[4] = "0"; //level 2 score
-                        currentSaveFile[5] = "Incomplete";  //level 3
-                        currentSaveFile[6] = "0"; //level 3 score
-                        currentSaveFile[7] = "Incomplete";  //level 4
-                        currentSaveFile[8] = "0"; //level 4 score
-                        saveFileFound = true; //stops the loop
-                        System.out.println("Save file created succesfully.");
-                        }
-                    }
-                userLogin = true;//stops the login loop
                 }
                 
-                
-                
-        }
+                saveFiles = fileModule.readFromFile(); //gets save datas
+                boolean saveFileFound = false;
+                while (!saveFileFound) {
+                    String[] tempSaveFile = new String[9]; //used to unpack each element of the list to check for the save data name
+                    for (int i = 0; i < saveFiles.size(); i++){
+                        tempSaveFile = saveFiles.get(i);
+                        if (tempSaveFile[0].equals(userName)){//if save data with user name exists
+                            System.out.println("Save file found. Importing data...");
+                            currentSaveFile = saveFiles.get(i);//assigns the save file data(array)
+                            saveFilePosition = i; //asigns the line number of the save data, so it can be overwritten later to save user's progress when they exit the app
+                        System.out.println("Save file imported succesfully.\n");
+                        saveFileFound = true;
+                        break;
+                    }
+                }
+                    
+                if (!saveFileFound){
+                    System.out.println("Save file wasn't found. Creating new save file...");
+                    //if cant find create a new array with the username and store it in the file
+                    currentSaveFile[0] = userName;
+                    currentSaveFile[1] = "Incomplete";  //level 1
+                    currentSaveFile[2] = "0";   //level 1 score
+                    currentSaveFile[3] = "Incomplete";  //level 2
+                    currentSaveFile[4] = "0"; //level 2 score
+                    currentSaveFile[5] = "Incomplete";  //level 3
+                    currentSaveFile[6] = "0"; //level 3 score
+                    currentSaveFile[7] = "Incomplete";  //level 4
+                    currentSaveFile[8] = "0"; //level 4 score
+                    saveFileFound = true; //stops the loop
+                    System.out.println("Save file created succesfully.");
+                    }
+                }
+            userLogin = true;//stops the login loop
+            }                        
+    }
     //going to need to put a variable in the () for a specific level
     public static void runLevel() {
         boolean userInLevelLoop = true;
@@ -164,7 +172,6 @@ public class MainMenu {
             }
         }
     }
-
     public static void main(String[] args) throws FileNotFoundException {
         /**
          * Main method which runs the app.
