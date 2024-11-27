239c8e92c3e9e2fd1835a13b078d4d98600bcb7c
Braknarhar300 <LSCHUETZ6519@smail.pcd.edu>
2024-11-27 13:59:13 -0800

    completed levels

---
+++ src/levelDesign/WorkInProgress2.java
@@ -0,0 +1,20 @@
+package levelDesign;
+
+import java.awt.*;
+
+public class WorkInProgress2 {
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(800, 400); // panel size
+        Graphics g = panel.getGraphics();
+
+
+        Rectangle rectangle1 = new Rectangle(350, 200, 100, 700);
+        Rectangle rectangle2 = new Rectangle(550, 0,100,200);
+
+        g.clearRect(0, 0, panel.getWidth(), panel.getHeight()); // background can be changed
+
+
+        rectangle1.draw(g);
+        rectangle2.draw(g);
+    }
+}
---
+++ src/levelDesign/WorkInprogress3.java
@@ -0,0 +1,20 @@
+package levelDesign;
+
+import java.awt.*;
+
+public class WorkInprogress3 {
+    public static void main(String[] args) {
+        DrawingPanel panel = new DrawingPanel(800, 400); // panel size
+        Graphics g = panel.getGraphics();
+
+
+        Rectangle rectangle1 = new Rectangle(350, 200, 100, 450);
+        Rectangle rectangle2 = new Rectangle(350, 0,100,150);
+
+        g.clearRect(0, 0, panel.getWidth(), panel.getHeight()); // background can be changed
+
+
+        rectangle1.draw(g);
+        rectangle2.draw(g);
+    }
+}
