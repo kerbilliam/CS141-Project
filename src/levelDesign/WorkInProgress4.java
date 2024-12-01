22bb51f929a4e4dde231bea56c890730c205d537
daniilnahl <daniil.nahl@protonmail.com>
2024-11-30 22:45:29 -0800

    Fixed panel redrawing
    Changed code in level design files so drawing panel and graphics are public variables.

--- src/levelDesign/WorkInProgress4.java
+++ src/levelDesign/WorkInProgress4.java
@@ -3,11 +3,10 @@ package levelDesign;
 import java.awt.*;
 
 public class WorkInProgress4 {
+    public static DrawingPanel panel = new DrawingPanel(800, 400); // panel size
+    public static Graphics g = panel.getGraphics();
+
     public static void levelFour() {
-        DrawingPanel panel = new DrawingPanel(800, 400); // panel size
-        Graphics g = panel.getGraphics();
-
-
         Rectangle rectangle1 = new Rectangle(350, 200, 100, 450);
         Rectangle rectangle2 = new Rectangle(350, 0, 100, 150);
         Rectangle rectangle3 = new Rectangle(550, 75, 100, 200);
